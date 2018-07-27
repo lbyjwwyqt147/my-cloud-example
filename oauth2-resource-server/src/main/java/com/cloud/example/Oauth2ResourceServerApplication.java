6c286d96a41d40b8d98f638a2e8687c8d74abac8
lbyjwwyqt147 <lbyjwwyqt147@163.com>
2018-07-27 17:15:10 +0800

    初始化工程

---
+++ oauth2-resource-server/src/main/java/com/cloud/example/Oauth2ResourceServerApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ResourceServerApplication.class, args);
+    }
+}
