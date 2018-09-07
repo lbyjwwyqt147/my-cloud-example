2b9e7dfa1a43adc63dac9c1ed488d8edc10d81e5
lbyjwwyqt147 <lbyjwwyqt147@163.com>
2018-09-07 17:19:53 +0800

    监控

--- user-server/src/main/java/com/cloud/example/user/UserServerApplication.java
+++ user-server/src/main/java/com/cloud/example/user/UserServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableHystrix
 @EnableFeignClients
+@EnableHystrixDashboard
 public class UserServerApplication {
 
     public static void main(String[] args) {
